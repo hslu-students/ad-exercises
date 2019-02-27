bb5330a5d79ae6f250aac201f36d3a288792a5b3
Benj Fassbind <randombenj@gmail.com>
2019-02-27 12:19:29 +0100

    Implement stack

--- src/main/java/ch/hslu/ad/exercise/Stack.java
+++ src/main/java/ch/hslu/ad/exercise/Stack.java
@@ -2,18 +2,39 @@ package ch.hslu.ad.exercise;
 
 
 public class Stack {
-  private String[] stack = new String[100];
+  private String[] stack;
   private int index = 0;
 
+  /**
+   * Returns the last element from the stack
+   */
   public String pop() {
     if (index == 0) {
       return null;
     } else {
-      return stack[index];
+      return stack[--index];
     }
   }
 
+  /**
+   * Adds a new element to the stack
+   */
   public void push(String element) {
+    stack[index++] = element;
+  }
 
+  /**
+   * Gets the number of elements
+   */
+  public int size() {
+    return index;
+  }
+
+  public Boolean isFull() {
+    return stack.length >= index;
+  }
+
+  public Stack(int size) {
+    this.stack = new String[size];
   }
 }
--- src/test/java/ch/hslu/ad/exercise/StackTest.java
+++ src/test/java/ch/hslu/ad/exercise/StackTest.java
@@ -9,7 +9,41 @@ class StackTest {
 
     @Test
     void testStackInitialization() {
-        final Stack stack = new Stack();
+        final Stack stack = new Stack(2);
         assertTrue(stack.pop() == null);
+    }
+
+    @Test
+    void testStackPush() {
+        final Stack stack = new Stack(2);
+        stack.push("HOOI");
+        assertTrue(stack.size() == 1);
+    }
+
+    @Test
+    void testStackPop() {
+        final Stack stack = new Stack(2);
+        stack.push("HOOI");
+        final String element = stack.pop();
+        assertTrue(stack.size() == 0);
+        assertTrue(element == "HOOI");
+    }
+
+    @Test
+    void testStackFull() {
+        final Stack stack = new Stack(1);
+        stack.push("HOOI");
+        assertTrue(stack.isFull());
+    }
+
+    @Test
+    void testStackMessage() {
+      final Stack stack = new Stack(3);
+      stack.push("toll");
+      stack.push("sind");
+      stack.push("Datenstrukturen");
+      assertTrue(stack.pop() == "Datenstrukturen");
+      assertTrue(stack.pop() == "sind");
+      assertTrue(stack.pop() == "toll");
     }
 }

2e9bdec362b3595cffe6f7221fe72d9a6c21c57e
Benj Fassbind <randombenj@gmail.com>
2019-06-23 15:29:05 +0200

    Add concurrent sort demo

--- src/main/java/ch/hslu/ad/sort/SortDemo.java
+++ src/main/java/ch/hslu/ad/sort/SortDemo.java
@@ -2,6 +2,10 @@ package ch.hslu.ad.sort;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+
+import ch.hslu.ad.concurreny.ForkJoinDemo;
 
 /**
  * SortDemo
@@ -28,5 +32,18 @@ public class SortDemo {
             System.out.println("Time needed: " + elapsed + "ms");
         });
 
+        // concurrent deomo
+        final ForkJoinPool pool = new ForkJoinPool();
+        final ConcurrentMergeSort<Integer> task = new ConcurrentMergeSort<Integer>(
+            Arrays.asList(1,3,4,9,0,2,7,8,5,6)
+        );
+        pool.execute(task);
+        System.out.printf("SORT USING [class ch.hslu.ad.sort.ConcurentMergeSort]");
+        try {
+			System.out.println(task.get());
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+        }
+        pool.shutdown();
     }
 }
